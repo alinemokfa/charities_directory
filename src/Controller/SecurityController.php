bffc28119bef41152265e8190ad07f82064bf194
Aline <alinemokfa@hotmail.co.uk>
2018-09-16 11:35:27 +0100

    Add Security controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+
+        $lastUsername = $utils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+    }
+}
